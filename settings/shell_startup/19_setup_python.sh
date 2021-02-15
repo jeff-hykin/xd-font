53951be4fb6ae681a3585fa5efaf94c01e287c99
Jeff Hykin <jeff.hykin@gmail.com>
2021-02-15 11:28:38 -0600

    - make the python command respond to venv

--- settings/shell_startup/19_setup_python.sh
+++ settings/shell_startup/19_setup_python.sh
@@ -3,7 +3,8 @@
 # and I can't figure out how to override the videoio setting
 # relevent link to the nix build-opencv: https://github.com/NixOS/nixpkgs/blob/193a6a2307b7b29aa11bee309d4aa41840686ab0/pkgs/development/libraries/opencv/4.x.nix#L258
 ls .venv &>/dev/null || python -m venv .venv
-source .venv/bin/activate
+export VIRTUAL_ENV="$PWD/.venv"
+export PATH="$PATH:$VIRTUAL_ENV/bin"
 
 function apt-ensure {
     required_pkg="$1"
