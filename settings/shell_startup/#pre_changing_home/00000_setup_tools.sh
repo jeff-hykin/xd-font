48bd451bf54eeff38b15a4e175610804d94cca8b
Jeff Hykin <jeff.hykin@gmail.com>
2021-03-31 10:14:34 -0500

    improve the injection method

--- settings/shell_startup/#pre_changing_home/00000_setup_tools.sh
+++ settings/shell_startup/#pre_changing_home/00000_setup_tools.sh
@@ -13,12 +13,25 @@ $1" >> .gitignore
 }
 
 function inject_into_path {
-    system_path="$(which "$1" 2>/dev/null)"
+    # if it is an absolute path use it directly
+    if [[ -f "$1" ]]
+    then
+        system_path="$1"
+    # if it is the name of a command, then find the path
+    else
+        system_path="$(which "$1" 2>/dev/null)"
+    fi
     # make sure its a file
     if [[ -f "$system_path" ]]; then
         mkdir -p ./settings/path_injection
-        # put it in the injections
-        ln -sf "$system_path" "./settings/path_injection"
+        # put it in the injections, and use the real home variable
+        new_executable="./settings/path_injection/$(basename "$system_path")"
+        echo "#!/usr/bin/env bash
+        HOME='$HOME' '$system_path' "'"$@"' > "$new_executable"
+        # copy over the execute permissions too
+        chmod --reference="$system_path" "$new_executable"
+        # make sure we can still read and write it though
+        chmod u+wr "$new_executable"
     else 
         echo "no system '$1' avalible for path injection (some stuff might break)"
     fi
